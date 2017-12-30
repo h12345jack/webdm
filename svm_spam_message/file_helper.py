7632a65ac1c33755e64c925e95f51de5959fe08f
Eadren King <eadrenking@gmail.com>
2017-12-30 15:28:45 +0800

    add file_helper to sovle path

---
+++ svm_spam_message/file_helper.py
@@ -0,0 +1,23 @@
+import os
+
+SVM_SPAM_MESSAGE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+def get_path(relative_path):
+    return os.path.join(SVM_SPAM_MESSAGE_DIR, relative_path)
+
+TRAINING_DATA_PATH = get_path('training_data.txt')
+TEST_DATA_PATH = get_path('test_data.txt')
+
+# tools path
+TFIDF_VZ_PATH = get_path('tool/tfidf_vectorizer.pkl')
+
+# data path
+TRAINING_FEATURE_MATRIX_PATH = get_path('data/training_feature_matrix.pkl')
+TRAINING_LABEL_PATH = get_path('data/training_labels.pkl')
+TEST_FEATURE_MATRIX_PATH = get_path('data/test_feature_matrix.pkl')
+TEST_LABEL_PATH = get_path('data/test_labels.pkl')
+
+
+# model path
+SVM_MODEL_PATH = get_path('model/svm.pkl')
+
