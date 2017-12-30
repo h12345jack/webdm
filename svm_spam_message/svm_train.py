225469d038bb97eb3ef89a1fe782d4099b48aca5
Eadren King <eadrenking@gmail.com>
2017-12-30 18:33:00 +0800

    add function comment

--- svm_spam_message/svm_train.py
+++ svm_spam_message/svm_train.py
@@ -16,18 +16,38 @@ from sklearn import svm
 from file_helper import SVM_MODEL_PATH
 
 def train_svm(feature_matrix, labels, save_model_path=SVM_MODEL_PATH):
+    """
+    train svm model with training feature matrix, labels. If model can
+    be trainned successfully, dump the model(classifier) into given path in
+    order to reuse in the future.
 
+    INPUT: training feature matrix, labels, save_model_path
+    OUTPUT: svm classifier model
+    """
     svm_clf = svm.LinearSVC()
     svm_clf.fit(feature_matrix, labels)
     joblib.dump(svm_clf, save_model_path)
 
     return svm_clf
 
+
 def get_trained_svm(model_path=SVM_MODEL_PATH):
+    """
+    load ever trainned svm classifier model from dumpped file
+
+    INPUT: None
+    OUTPUT: svm classifier model
+    """
     return joblib.load(model_path)
 
 
 def test_clf(clf, test_data, test_labels):
+    """
+    test svm model for test data. report the test result
+
+    INPUT: svm classifier, test feature matrix, test labels
+    OUTPUT: None
+    """
     predict_result = clf.predict(test_data)
     print("Classification report for classifier %s:\n%s\n" % (
         clf, metrics.classification_report(test_labels, predict_result, digits=4)))
